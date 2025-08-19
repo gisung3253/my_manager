69056dbef728e4c8721a1f902a6c9c5a9fa8c12a
gisung3253 <gisung3253@naver.com>
2025-08-20 01:47:24 +0900

    000 start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center">
       <h1 className="text-4xl font-bold text-blue-600">
-        김희찬 = 노무현
+        스타트
       </h1>
     </main>
   )
