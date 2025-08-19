55dca63b716979023e8723143f8dfa44917e9791
gisung3253 <gisung3253@naver.com>
2025-08-20 04:41:23 +0900

    eslint 규칙 끄기

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // 빌드 시 ESLint 검사 건너뛰기
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
